3c5dae726f60d6178bf0f1fded2c6a6a8508f21c
Азиза Амануллина <zizowca@gmail.com>
2022-01-09 21:45:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+        static void Factorial(int n)
+        {
+            int f = 1;
+            for (int i = 1; i < n+1; i++)
+            {
+                f *= i;
+                Thread.Sleep(20);
+            }
+            Console.WriteLine($"Факториал числа n= {f}");
+            
+        }
+        static async void FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
+    }
+}
---
+++ Задание 23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задание_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение n:");
+            int n = Convert.ToInt32(Console.ReadKey());
+            //Console.WriteLine(Factorial(n));
+            Console.ReadKey();
+        }
+        //static int Factorial(int n)
+        //{
+           
+        //    if (n == 1) return 1;
+        //    return n * Factorial(n - 1);
+
+        //}
+    }
+}
